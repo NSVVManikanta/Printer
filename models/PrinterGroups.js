df9b47c907ac9acba5a30a426ce144ddb9c1ceac
NSVVManikanta <101240994+NSVVManikanta@users.noreply.github.com>
2022-04-22 19:56:04 +0530

    Add files via upload

---
+++ models/PrinterGroups.js
@@ -0,0 +1,73 @@
+
+var  commonHelper = require('../Helper');
+module.exports = (sequelize,Sequelize)=>{
+const printerGroups= sequelize.define(
+    'PrinterGroups',{
+    id:{
+        type : Sequelize.INTEGER,
+        autoIncrement : true,
+        primaryKey : true,
+        field: 'c1',
+    },
+    title:{
+        type : Sequelize.STRING,
+        allowNull : false,
+        field:'c2',
+    },
+    description:{
+        type : Sequelize.STRING,
+        allowNull : true,
+        field:'c3',
+    },
+    printType:{
+        type : Sequelize.ENUM(1,2),
+        field:'c4', 
+    },
+    printerCount:{
+        type : Sequelize.INTEGER,
+        field:'c5',
+    },
+    createdAt:{
+        type: Sequelize.INTEGER,
+        allowNull : false,
+        field:'c6',
+    },
+    updatedAt:{
+        type: Sequelize.INTEGER,
+        allowNull : false,
+        field:'c7',
+    },
+    createdBy:{
+        type: Sequelize.INTEGER,
+        allowNull : false,
+        field:'c8',
+    },
+    updatedBy:{
+        type: Sequelize.INTEGER,
+        allowNull : false,
+        field:'D9',
+    }
+   
+},
+{
+tableName: 't109',
+hasTrigger: true,
+hooks: {
+    beforeCreate(printerGroupObject, options){
+        if(!printerGroupObject) 
+        return printerGroupObject.createdAt =commonHelper.getTimeStamp();
+    },
+    beforeUpdate(printerGroupObject, options){
+        if(!printerGroupObject) 
+        return printerGroupObject.updatedAt =commonHelper.getTimeStamp();
+    },
+  },
+},
+);
+// printerGroups.associate = function(models){
+//     printerGroups.belongsTo(models.printerGroupTriggers,{foreignkey:"printerGroupId"})
+// }
+printerGroups.hasMany(printerGroupTriggers,{ foreignKey: 'printerGroupId' });
+printerGroupTriggers.belongsTo(printerGroups);
+return printerGroups;
+}
