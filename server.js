d193557aad32842e12975ccca707d402d9618b0f
NSVVManikanta <101240994+NSVVManikanta@users.noreply.github.com>
2022-04-25 20:19:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express');
-// const printerGroups = require('./models/PrinterGroups');
-// const printerGroupTriggers = require('./models/PrinterGroupTriggers');
- const sequelize = require('./util/database');
+const printerGroups = require('./models/PrinterGroups');
+const triggers = require('./models/triggers');
+const db = require('./util/database');
  const route = require('./Routes/route');
+const swaggerUi = require('swagger-ui-express');
+const yaml = require('yamljs');
+const swaggerDocs = yaml.load('./swagger.yaml');
 const app = express();
-
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocs));
 
 app.use(express.json());
 
+ db.sequelize
+  .sync()
+  .then(()=>{
+      console.log("Database and tables created!");
+  }).catch(err=>console.log(err.original));
 
-
+app.use('/',route);
 
 app.listen(8080,()=>{
     console.log('Sever running on 8080!');
